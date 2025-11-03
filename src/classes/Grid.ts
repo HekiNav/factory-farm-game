e2a2834c1e7e4cc569c375c9d6c749463e0b7949
Unto Ahti <developer.hekinav@gmail.com>
2025-11-03 17:00:59 +0200

    building base classes

--- src/classes/Grid.ts
+++ src/classes/Grid.ts
@@ -1,7 +1,24 @@
-import { Vector2 } from "./Utils";
+import { Tile } from "./Tile.js";
 
 export default class Grid {
-    constructor (width: number, height: number, ) {
-        
+    gridData: Array<Array<Tile>>
+    width: number
+    height: number
+    tileSize: number
+    constructor(width: number, height: number, tileSize: number) {
+        this.gridData = this.#generateGrid(width, height, tileSize)
+        this.width = width
+        this.height = height
+        this.tileSize = tileSize
+    }
+    #generateGrid(width: number, height: number, _tileSize: number): Array<Array<Tile>> {
+        const tiles = new Array<Array<Tile>>()
+        for (let i = 0; i < height; i++) {
+            tiles.push([])
+            for (let j = 0; j < width; i++) {
+                tiles[i].push(new Tile())
+            }
+        }
+        return tiles
     }
 }
--- src/classes/Utils.ts
+++ src/classes/Utils.ts
@@ -1,8 +1,38 @@
 export class Vector2 {
     x: number
     y: number
-    constructor(x: number, y? : number) {
+    constructor(x: number, y: number) {
         this.x = x
-        this.y = y || x
+        this.y = y
+    }
+    multiply(b: Vector2) {
+        Vector2.multiply(this,b)
+    }
+    static multiply(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x * b.x, a.y * b.y)
+    }
+    divide(b: Vector2) {
+        Vector2.divide(this,b)
+    }
+    static divide(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x / b.x, a.y / b.y)
+    }
+    add(b: Vector2) {
+        Vector2.add(this,b)
+    }
+    static add(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x + b.x, a.y + b.y)
+    }
+    subtract(b: Vector2) {
+        Vector2.add(this,b)
+    }
+    static subtract(a: Vector2, b: Vector2): Vector2 {
+        return new Vector2(a.x - b.x, a.y - b.y)
+    }
+    equals(b: Vector2) {
+        Vector2.equals(this,b)
+    }
+    static equals(a: Vector2, b: Vector2): boolean {
+        return a.x === b.x && a.y === b.y
     }
 }
